69aef17ef625acfff488767997861113fcc81db7
richBi <richbi@RichB>
2016-06-28 00:14:27 -0500

    Hola Mundo desde Node

---
+++ hola-mundo-en-node.js
@@ -0,0 +1,12 @@
+var express = require('express'); // require=import
+var aplicacion = express();
+
+aplicacion.get('/', function(req, res){
+	/*setTimeout(function(){
+		res.send('Hello World...');
+	}, 2000);*///demora en cargar 2 segundos
+	res.send('Hello World... desde Node.Js');
+});
+
+aplicacion.listen(3000);
+console.log('Servidor corriendo en LOCALHOST...')
